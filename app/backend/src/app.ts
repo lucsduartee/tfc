7042c39c8a112df544bdaa37a388b090f1dfdf16
Lucas Duarte <lucs.rkt@gmail.com>
2022-06-04 01:09:24 -0300

    feat: make app use loginRouter

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import loginRouter from './routes/Login.route';
 
 class App {
   public app: express.Express;
@@ -19,6 +20,7 @@ class App {
     };
 
     this.app.use(accessControl);
+    this.app.use('/login', loginRouter);
   }
 
   // ...
