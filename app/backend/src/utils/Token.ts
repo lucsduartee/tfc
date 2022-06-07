9192ab42027502aa2b06b554ed3f5f8460da2783
Lucas Duarte <lucs.rkt@gmail.com>
2022-06-06 23:02:09 -0300

    feat: created Token utils

---
+++ app/backend/src/utils/Token.ts
@@ -0,0 +1,24 @@
+import * as jwt from 'jsonwebtoken';
+import * as fs from 'fs/promises';
+
+type LoginData = {
+  email: string,
+  passwordRaw: string,
+};
+
+export default class Token {
+  static async getKey(): Promise<string> {
+    const masterKey = await fs.readFile('./jwt.evaluation.key', 'utf-8');
+    return masterKey;
+  }
+
+  static async generate(data: LoginData): Promise<string> {
+    const masterKey = await Token.getKey();
+    const token = jwt.sign(data, masterKey, {
+      expiresIn: '1d',
+      algorithm: 'HS256',
+    });
+
+    return token;
+  }
+}
