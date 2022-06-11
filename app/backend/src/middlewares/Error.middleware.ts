e484759b6a54c08d65e618d2b60fb938d7ab5b5d
Lucas Duarte <lucs.rkt@gmail.com>
2022-06-11 15:34:36 -0300

    feat: created class ErrorMiddleware

---
+++ app/backend/src/middlewares/Error.middleware.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import Errors from '../enums/Errors';
+import ICustomError from '../interfaces/ICustomError';
+
+export default class ErrorMiddleware {
+  static async validate(
+    err: unknown,
+    _req: Request,
+    res: Response,
+    _next: NextFunction,
+  ) {
+    console.log(err);
+    if ((err as ICustomError).code) {
+      return res.status((err as ICustomError).code)
+        .json({ message: (err as ICustomError).message });
+    }
+
+    return res.status(Errors.INTERNAL_SERVER_ERROR)
+      .json({ message: 'Internal Server Error' });
+  }
+}
