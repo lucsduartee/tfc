5009382ca0e2d5741f06a2ce103bcde0e53a0a6e
Lucas Duarte <lucs.rkt@gmail.com>
2022-06-21 02:29:18 -0300

    feat: created Matches controller

---
+++ app/backend/src/controllers/Matches.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express';
+import MatchesService from '../services/Matches.service';
+
+export default class MatchesController {
+  public matchesService: MatchesService;
+
+  constructor(matchesService: MatchesService) {
+    this.matchesService = matchesService;
+  }
+
+  async getAll(_req: Request, res: Response, next: NextFunction) {
+    try {
+      const matches = await this.matchesService.getAll();
+      console.log(matches);
+      return res.status(200).json(matches);
+    } catch (err) {
+      return next(err);
+    }
+  }
+}
